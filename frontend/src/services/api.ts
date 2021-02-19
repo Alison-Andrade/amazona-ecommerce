8b5db9de0f1356b2f548b875a12bac2f11fc396d
Alison Andrade <sidneialison@gmail.com>
2021-02-19 12:45:08 -0300

    Publish-To-Heroku

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
+const port = process.env.PORT || 3333
+
 const api = axios.create({
-    baseURL: 'http://localhost:3333',
+    baseURL: `http://localhost/${port}`,
 })
 
 export default api;
