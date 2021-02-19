fcf939c5f56634d399abe5ded4b361abda61828e
Alison Andrade <sidneialison@gmail.com>
2021-02-19 13:08:15 -0300

    Publish-To-Heroku

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
-const api = process.env.NODE_ENV === "development" ? axios.create({ baseURL: 'http://localhost/3333', })
-    : process.env.NODE_ENV === "production" && axios.create({ baseURL: 'https://my-amazona-ecommerce.herokuapp.com/', })
+const api = process.env.NODE_ENV === "development" ? axios.create({ baseURL: 'http://localhost/3333', }) : axios.create({ baseURL: 'https://my-amazona-ecommerce.herokuapp.com/', })
 
 
 export default api
